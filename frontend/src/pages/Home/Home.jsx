0e640cf09b21895e94fccd414750ffc55702f8d6
Divya Kumar Singh <dksingh8957@gmail.com>
2025-06-25 12:42:51 +0530

    frontend

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import'./Home.css'
 import Header from '../../component/Header/Header'
+import ExploreMenu from '../../component/ExploreMenu/ExploreMenu'
+import FoodDisoplay from '../../component/FoodDisplay/FoodDisoplay'
 
 const Home = () => {
+
+    const [category, setCategory] = useState("ALL");
+
+
   return (
     <div>
       <Header/>
+      <ExploreMenu category={category} setCategory={setCategory}/>
+      <FoodDisoplay category={category}/>
     </div>
   )
 }
