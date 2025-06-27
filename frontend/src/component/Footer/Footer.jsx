df21d20a3a57c9c9c907f8ed91524d161290abbb
Divya Kumar Singh <dksingh8957@gmail.com>
2025-06-27 16:46:01 +0530

    frontend complete

--- frontend/src/component/Footer/Footer.jsx
+++ frontend/src/component/Footer/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
     <div className="footer-content">
         <div className="footer-content-left">
             <img src={assets.logo} alt="" />
-            <p>dddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd</p>
+            <p>Choose from a deverse menu featuring a delectable array of dishes crafted with the finest ingredients and culinary expertise. Our mission is to satify your cravings and elevate your dining experirence, one delicious meal at a time.</p>
             <div className="footer-social-icons">
                 <img src={assets.facebook_icon} alt="" />
                 <img src={assets.twitter_icon} alt="" />
